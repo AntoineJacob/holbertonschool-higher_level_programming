bd248e7ac1e3c131bc2b439c7e9630ff752015f3
AntoineJacob <5646@holbertonstudents.com>
2023-01-26 05:56:43 -0800

    Task 1

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def search_replace(my_list, search, replace):
+    new_list = []
+    if my_list:
+        new_list = list(map(lambda x: if x != search else replace, my_list))
+    return new_list
