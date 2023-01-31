85ecaaf6b0ecdf92e5ce670168ee7bc4246f7008
AntoineJacob <5646@holbertonstudents.com>
2023-01-31 04:07:29 -0800

    Adding readme and task 0

---
+++ python-classes/0-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This is the square size def
+"""
+
+class Square:
+    """
+    The Square class with the size attribute
+    """
+
+    def __init__(self):
+        """
+        Args:
+            any args of the square
+        """
