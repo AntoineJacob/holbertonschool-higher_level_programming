2c4f7cc6e71b52f95f8b34050afba596fde8218a
AntoineJacob <5646@holbertonstudents.com>
2023-01-31 05:04:35 -0800

    Task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This is the square size def
+"""
+
+
+class Square:
+    """
+    Square class with private instance attribute size
+    """
+
+    def __init__(self, size=0):
+        """
+        Args:
+            size of the of the square
+        """
+        self.__size = size
