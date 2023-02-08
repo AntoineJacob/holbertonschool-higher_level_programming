b284e49f7ee7376d7d68ed09656f82395ec8b54e
AntoineJacob <5646@holbertonstudents.com>
2023-02-08 07:51:49 -0800

    adding task 1 and test for task 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module my_list
+"""
+
+
+class Mylist(list):
+    """
+    The Mylist class who inherits from list
+    """
+def print_sorted(self):
+    print(sorted(self))
