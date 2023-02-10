80c34116cb4b90591c65f488350936de2f186403
AntoineJacob <5646@holbertonstudents.com>
2023-02-10 12:47:51 +0100

    fix doc v1

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,7 +7,6 @@ to a file
 import json
 from sys import argv
 
-
 save_json = __import__('7-save_to_json_file').save_to_json_file
 load_json = __import__('8-load_from_json_file').load_from_json_file
 
