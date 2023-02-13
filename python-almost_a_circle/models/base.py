26bf1f9f64fea2f7ce6b7d321227c182a05e5e9a
AntoineJacob <5646@holbertonstudents.com>
2023-02-13 14:02:45 +0100

    fixing pep8

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -3,12 +3,14 @@
 In this module, there is the base class
 """
 
+
 class Base:
     """
-    The class base, who gonna be our model for 
+    The class base, who gonna be our model for
     the whole project
     """
     __nb_objects = 0
+    
     def __init__(self, id=None):
         """
         Our init method
