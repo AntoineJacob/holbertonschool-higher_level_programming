11af927507ebb90231e44b9f50473550ca46bbaf
AntoineJacob <5646@holbertonstudents.com>
2023-02-13 14:00:17 +0100

    adding README and the base and init files

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+In this module, there is the base class
+"""
+
+class Base:
+    """
+    The class base, who gonna be our model for 
+    the whole project
+    """
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """
+        Our init method
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
