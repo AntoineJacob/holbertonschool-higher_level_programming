49d71adf81b1e80e75b594b6c0e6aac46343684b
AntoineJacob <5646@holbertonstudents.com>
2023-02-14 15:31:37 +0100

    task 16

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -31,3 +31,17 @@ class Base:
         if list_dictionaries is None:
             list_dictionaries = []
         return (json.dumps(list_dictionaries))
+
+    def save_to_file(cls, list_objs):
+        """
+        This function writes the json
+        string representation of list_objs
+        to a file
+        """
+        jf = cls.__name__ + ".json"
+        obj = []
+        if list_objs is not None:
+            for i in list_objs:
+                obj.append(cls.to_dictionary(i))
+        with open(jf, mode="w") as myFile:
+            myFile.write(cls.to_json_string(obj))
