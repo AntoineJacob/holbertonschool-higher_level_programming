de4ba68b3a764d3c9317455453e9fa9ddbc267a5
AntoineJacob <5646@holbertonstudents.com>
2023-02-14 15:34:05 +0100

    end task

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -3,6 +3,8 @@
 In this module, there is the base class
 """
 import json
+import csv
+
 
 
 class Base:
@@ -32,16 +34,95 @@ class Base:
             list_dictionaries = []
         return (json.dumps(list_dictionaries))
 
+    @classmethod
     def save_to_file(cls, list_objs):
         """
         This function writes the json
         string representation of list_objs
         to a file
         """
-        jf = cls.__name__ + ".json"
-        obj = []
+        fn = cls.__name__ + ".json"
+        ob = []
         if list_objs is not None:
             for i in list_objs:
-                obj.append(cls.to_dictionary(i))
-        with open(jf, mode="w") as myFile:
-            myFile.write(cls.to_json_string(obj))
+                ob.append(cls.to_dictionary(i))
+        with open(fn, mode="w") as myFile:
+            myFile.write(cls.to_json_string(ob))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        This function returns the list
+        of the json string represenation
+        """
+        if json_string is None or len(json_string) == 0:
+            return ([])
+        return (json.loads(json_string))
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        This function returns an instance
+        with all attributes already set
+        """
+        if cls.__name__ is "Rectangle":
+            temp = cls(1, 1)
+        if cls.__name__ is "Square":
+            temp = cls(1)
+        temp.update(**dictionary)
+        return (temp)
+
+    @classmethod
+    def load_from_file(cls):
+        """
+        This function returns a list
+        of instances
+        """
+        fn = cls.__name__ + ".json"
+        lst = []
+        try:
+            with open(fn, mode="r") as myFile:
+                lst = cls.from_json_string(myFile.read())
+            for i, j in enumerate(lst):
+                lst[i] = cls.create(**lst[i])
+        except:
+            pass
+        return (lst)
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """
+        This function serializes in csv
+        """
+        fn = cls.__name__ + ".csv"
+        if fn == "Rectangle.csv":
+            fields = ["id", "width", "height", "x", "y"]
+        else:
+            fields = ["id", "size", "x", "y"]
+        with open(fn, mode="w", newline="") as myFile:
+            if list_objs is None:
+                writer = csv.writer(myFile)
+                writer.writerow([[]])
+            else:
+                writer = csv.DictWriter(myFile, fieldnames=fields)
+                writer.writeheader()
+                for x in list_objs:
+                    writer.writerow(x.to_dictionary())
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """
+        This function deserializes from csv
+        """
+        try:
+            fn = cls.__name__ + ".csv"
+            with open(fn, newline="") as myFile:
+                reader = csv.DictReader(myFile)
+                lst = []
+                for x in reader:
+                    for i, n in x.items():
+                        x[i] = int(n)
+                    lst.append(x)
+                return ([cls.create(**objt) for objt in lst])
+        except FileNotFoundError:
+            return ([])
