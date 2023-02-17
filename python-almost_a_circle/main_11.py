9e193b43742b2a8c19032906e1bffcf985b7a078
AntoineJacob <5646@holbertonstudents.com>
2023-02-17 13:05:13 +0100

    fixing bug

--- python-almost_a_circle/main_11.py
+++ python-almost_a_circle/main_11.py
@@ -1,85 +1,29 @@
 #!/usr/bin/python3
-""" Doc """
-import os, sys
-import subprocess
+""" 11-main """
+from models.square import Square
 
+if __name__ == "__main__":
 
-def run_command(cmd):
-    process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output, error = process.communicate()
-    return "{}{}".format(output, error)
+    s1 = Square(5)
+    print(s1)
 
+    s1.update(10)
+    print(s1)
 
-def run_unittest():
-    nb_tests = 0
-    is_success = False
-    try:
-        res = run_command("python3 -m unittest discover tests")
-        for line in res.split("\\n"):
-            if "Ran " in line:
-                nb_tests = int(res.split("Ran ")[-1].split(" tests")[0])
-            if nb_tests > 0 and "OK" in line:
-                is_success = True
-    except:
-        nb_tests = 0
-        is_success = False
-    return nb_tests, is_success
+    s1.update(1, 2)
+    print(s1)
 
+    s1.update(1, 2, 3)
+    print(s1)
 
-# validate tests are passing by default
-nb_tests, passing = run_unittest()
+    s1.update(1, 2, 3, 4)
+    print(s1)
 
-if nb_tests <= 0:
-    print("No test found")
-    exit(1)
-if not passing:
-    print("Regular tests are not passing")
-    exit(1)
+    s1.update(x=12)
+    print(s1)
 
-file_path_to_update = "models/rectangle.py"
-file_path_updated = "models/tmp_rectangle.py"
-if not os.path.exists(file_path_to_update):
-    print("{} not found".format(file_path_to_update))
-    exit(1)
+    s1.update(size=7, y=1)
+    print(s1)
 
-try:
-    # Move file
-    if os.path.exists(file_path_updated):
-        os.remove(file_path_updated)
-    os.rename(file_path_to_update, file_path_updated)
-
-    # update file
-    new_content = """#!/usr/bin/python3
-\"\"\" Random documentation \"\"\"
-from models.tmp_rectangle import Rectangle
-
-
-class Rectangle(Rectangle):
-    \"\"\" Random documentation \"\"\"
-
-    def __init__(self, width, height, x=0, y=0, id=None):
-        \"\"\" Random documentation \"\"\"
-        if x == 0 and y == 0 and id is None:
-            width, height = height, width
-        super().__init__(width, height, x, y, id)
-"""
-
-    with open(file_path_to_update, "w") as file:
-        file.write(new_content)
-
-    # run tests
-    nb_tests, passing = run_unittest()
-    if nb_tests <= 0:
-        print("No test found")
-    if passing:
-        print("No test found for this case")
-except:
-    print("An error occured... {}".format(sys.exc_info()[0]))
-
-# rollback file
-if os.path.exists(file_path_updated):
-    if os.path.exists(file_path_to_update):
-        os.remove(file_path_to_update)
-    os.rename(file_path_updated, file_path_to_update)
-
-print("OK", end="")
+    s1.update(size=7, id=89, y=1)
+    print(s1)
--- python-almost_a_circle/main_2.py
+++ python-almost_a_circle/main_2.py
@@ -1,86 +1,27 @@
 #!/usr/bin/python3
-""" Doc """
-import os, sys
-import subprocess
+""" 2-main """
+from models.rectangle import Rectangle
 
+if __name__ == "__main__":
 
-def run_command(cmd):
-    process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output, error = process.communicate()
-    return "{}{}".format(output, error)
-
-
-def run_unittest():
-    nb_tests = 0
-    is_success = False
     try:
-        res = run_command("python3 -m unittest discover tests")
-        for line in res.split("\\n"):
-            if "Ran " in line:
-                nb_tests = int(res.split("Ran ")[-1].split(" tests")[0])
-            if nb_tests > 0 and "OK" in line:
-                is_success = True
-    except:
-        nb_tests = 0
-        is_success = False
-    return nb_tests, is_success
+        Rectangle(10, float('nan'))
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
 
+    try:
+        r = Rectangle(10, 2)
+        r.width = -10
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
 
-# validate tests are passing by default
-nb_tests, passing = run_unittest()
+    try:
+        r = Rectangle(10, 2)
+        r.x = {}
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
 
-if nb_tests <= 0:
-    print("No test found")
-    exit(1)
-if not passing:
-    print("Regular tests are not passing")
-    exit(1)
-
-file_path_to_update = "models/base.py"
-file_path_updated = "models/tmp_base.py"
-if not os.path.exists(file_path_to_update):
-    print("{} not found".format(file_path_to_update))
-    exit(1)
-
-try:
-    # Move file
-    if os.path.exists(file_path_updated):
-        os.remove(file_path_updated)
-    os.rename(file_path_to_update, file_path_updated)
-
-    # update file
-    new_content = """#!/usr/bin/python3
-\"\"\" Random documentation \"\"\"
-from models.tmp_base import Base
-
-
-class Base(Base):
-    \"\"\" Random documentation \"\"\"
-
-    def __init__(self, id=None):
-        \"\"\" Random documentation \"\"\"
-        if id is not None:
-            self.id = (id + 1)
-        else:
-            super().__init__(id)
-"""
-
-    with open(file_path_to_update, "w") as file:
-        file.write(new_content)
-
-    # run tests
-    nb_tests, passing = run_unittest()
-    if nb_tests <= 0:
-        print("No test found")
-    if passing:
-        print("No test found for this case")
-except:
-    print("An error occured... {}".format(sys.exc_info()[0]))
-
-# rollback file
-if os.path.exists(file_path_updated):
-    if os.path.exists(file_path_to_update):
-        os.remove(file_path_to_update)
-    os.rename(file_path_updated, file_path_to_update)
-
-print("OK", end="")
+    try:
+        Rectangle(10, 2, 3, -1)
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
