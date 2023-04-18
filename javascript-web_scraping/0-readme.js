4f92007e2944650db08b50ec516810ac69c20819
AntoineJacob <5646@holbertonstudents.com>
2023-04-18 00:49:16 -0700

    readme and task 0

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const rd = require('rd');
+
+rd.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    process.stdout.write(data);
+  }
+});
